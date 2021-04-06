c963d9b9fe58ad015ca1b60bdd00336f6ccf2d75
Mikhail Akopov <mikhail.akopov@getid.ee>
2021-04-06 15:00:47 +0300

    Add a way to manually upgrade a request to WebSocket

--- Sources/Vapor/Routing/RoutesBuilder+WebSocket.swift
+++ Sources/Vapor/Routing/RoutesBuilder+WebSocket.swift
@@ -51,13 +51,25 @@ extension RoutesBuilder {
         onUpgrade: @escaping (Request, WebSocket) -> ()
     ) -> Route {
         return self.on(.GET, path) { request -> Response in
-            let res = Response(status: .switchingProtocols)
-            res.upgrader = .webSocket(maxFrameSize: maxFrameSize, shouldUpgrade: {
-                shouldUpgrade(request)                
-            }, onUpgrade: { ws in
-                onUpgrade(request, ws)
-            })
-            return res
+            return request.webSocket(maxFrameSize: maxFrameSize, shouldUpgrade: shouldUpgrade, onUpgrade: onUpgrade)
         }
+    }
+}
+
+extension Request {
+    public func webSocket(
+        maxFrameSize: WebSocketMaxFrameSize = .`default`,
+        shouldUpgrade: @escaping ((Request) -> EventLoopFuture<HTTPHeaders?>) = {
+            $0.eventLoop.makeSucceededFuture([:])
+        },
+        onUpgrade: @escaping (Request, WebSocket) -> ()
+    ) -> Response {
+        let res = Response(status: .switchingProtocols)
+        res.upgrader = .webSocket(maxFrameSize: maxFrameSize, shouldUpgrade: {
+            shouldUpgrade(self)
+        }, onUpgrade: { ws in
+            onUpgrade(self, ws)
+        })
+        return res
     }
 }
